46ac64f22b91e51232e6a58c57ef2e254993826a
UO265181 <UO265181@uniovi.es>
2023-04-14 12:16:59 +0200

    Avances en TypeChecking

--- Esqueleto Traductor/src/semantic/TypeChecking.java
+++ Esqueleto Traductor/src/semantic/TypeChecking.java
@@ -44,37 +44,42 @@ import visitor.*;
 
 public class TypeChecking extends DefaultVisitor {
 
-    public TypeChecking(ErrorManager errorManager) {
-        this.errorManager = errorManager;
-    }
+	public TypeChecking(ErrorManager errorManager) {
+		this.errorManager = errorManager;
+	}
 
-	//	class DefinitionVariable { String name;  Type type; }
+	// class DefinitionVariable { String name; Type type; }
 	public Object visit(DefinitionVariable node, Object param) {
 		super.visit(node, param);
 
-		predicado(node.getType()!=TypeVoid.getInstance(), "No se puede definir una variable de tipo void: " + node.getName(), node);
+		predicado(node.getType() != TypeVoid.getInstance(),
+				"No se puede definir una variable de tipo void: " + node.getName(), node);
 
 		return null;
 	}
 
-	//	class DefinitionFunction { String name;  List<DefinitionVariable> definitionFunctionParams;  Type type;  List<DefinitionVariable> localVariables;  List<Sentence> sentences; }
+	// class DefinitionFunction { String name; List<DefinitionVariable>
+	// definitionFunctionParams; Type type; List<DefinitionVariable> localVariables;
+	// List<Sentence> sentences; }
 	public Object visit(DefinitionFunction node, Object param) {
 
 		for (DefinitionVariable child : node.getDefinitionFunctionParams()) {
 			child.accept(this, param);
-			predicado(child.getType().isPrimitive(), "El tipo de las variables de una función ha de ser primitivo: " + node.getName(), node);
+			predicado(child.getType().isPrimitive(),
+					"El tipo de las variables de una función ha de ser primitivo: " + node.getName(), node);
 		}
-			
+
 		node.getType().accept(this, param);
 
-		if(node.getType() != TypeVoid.getInstance()) {
-			predicado(node.getType().isPrimitive(), "El tipo de retorno de una función ha de ser primitivo: " + node.getName(), node);
-		}	
+		if (node.getType() != TypeVoid.getInstance()) {
+			predicado(node.getType().isPrimitive(),
+					"El tipo de retorno de una función ha de ser primitivo: " + node.getName(), node);
+		}
 
-        for (DefinitionVariable child : node.getLocalVariables()) {
+		for (DefinitionVariable child : node.getLocalVariables()) {
 			child.accept(this, param);
 		}
-				
+
 		for (Sentence child : node.getSentences()) {
 			child.setFatherFunction(node);
 			child.accept(this, param);
@@ -83,7 +88,7 @@ public class TypeChecking extends DefaultVisitor {
 		return null;
 	}
 
-	//	class StructField { String name;  Type type; }
+	// class StructField { String name; Type type; }
 	public Object visit(StructField node, Object param) {
 
 		// super.visit(node, param);
@@ -94,27 +99,27 @@ public class TypeChecking extends DefaultVisitor {
 		return null;
 	}
 
-	//	class TypeInt {  }
+	// class TypeInt { }
 	public Object visit(TypeInt node, Object param) {
 		return null;
 	}
 
-	//	class TypeFloat {  }
+	// class TypeFloat { }
 	public Object visit(TypeFloat node, Object param) {
 		return null;
 	}
 
-	//	class TypeChar {  }
+	// class TypeChar { }
 	public Object visit(TypeChar node, Object param) {
 		return null;
 	}
 
-	//	class TypeVoid {  }
+	// class TypeVoid { }
 	public Object visit(TypeVoid node, Object param) {
 		return null;
 	}
 
-	//	class TypeArray { ExpressionConstantInt size;  Type type; }
+	// class TypeArray { ExpressionConstantInt size; Type type; }
 	public Object visit(TypeArray node, Object param) {
 
 		// super.visit(node, param);
@@ -128,12 +133,12 @@ public class TypeChecking extends DefaultVisitor {
 		return null;
 	}
 
-	//	class TypeStruct { String name; }
+	// class TypeStruct { String name; }
 	public Object visit(TypeStruct node, Object param) {
 		return null;
 	}
 
-	//	class SentencePrint { Expression expression; }
+	// class SentencePrint { Expression expression; }
 	public Object visit(SentencePrint node, Object param) {
 
 		// super.visit(node, param);
@@ -144,7 +149,7 @@ public class TypeChecking extends DefaultVisitor {
 		return null;
 	}
 
-	//	class SentencePrintsp { Expression expression; }
+	// class SentencePrintsp { Expression expression; }
 	public Object visit(SentencePrintsp node, Object param) {
 
 		// super.visit(node, param);
@@ -155,7 +160,7 @@ public class TypeChecking extends DefaultVisitor {
 		return null;
 	}
 
-	//	class SentencePrintln { Expression expression; }
+	// class SentencePrintln { Expression expression; }
 	public Object visit(SentencePrintln node, Object param) {
 
 		// super.visit(node, param);
@@ -166,18 +171,29 @@ public class TypeChecking extends DefaultVisitor {
 		return null;
 	}
 
-	//	class SentenceReturn { Expression expression; }
+	// class SentenceReturn { Expression expression; }
 	public Object visit(SentenceReturn node, Object param) {
+		super.visit(node, param);
 
-		// super.visit(node, param);
+		if (node.getExpression() == null) {
+			predicado(node.getFatherFunction().getType() == TypeVoid.getInstance(),
+					"Esta función tiene tipo de retorno y ha de devolver un valor: " + node.getFatherFunction().getName(),
+					node.getFatherFunction());
+		} else {
 
-		if (node.getExpression() != null)
-			node.getExpression().accept(this, param);
+			predicado(node.getFatherFunction().getType() != TypeVoid.getInstance(),
+					"El retorno de una función void no puede devolver un valor: " + node.getFatherFunction().getName(),
+					node);
+			predicado(node.getExpression().getType() == node.getFatherFunction().getType(),
+					"El tipo de retorno ha de coincidir con el tipo de la función: "
+							+ node.getFatherFunction().getName(),
+					node);
+		}
 
 		return null;
 	}
 
-	//	class SentenceRead { Expression expression; }
+	// class SentenceRead { Expression expression; }
 	public Object visit(SentenceRead node, Object param) {
 
 		// super.visit(node, param);
@@ -188,7 +204,7 @@ public class TypeChecking extends DefaultVisitor {
 		return null;
 	}
 
-	//	class SentenceAssignment { Expression left;  Expression right; }
+	// class SentenceAssignment { Expression left; Expression right; }
 	public Object visit(SentenceAssignment node, Object param) {
 
 		// super.visit(node, param);
@@ -202,7 +218,8 @@ public class TypeChecking extends DefaultVisitor {
 		return null;
 	}
 
-	//	class SentenceCallFunction { String name;  List<Expression> callFunctionParams; }
+	// class SentenceCallFunction { String name; List<Expression>
+	// callFunctionParams; }
 	public Object visit(SentenceCallFunction node, Object param) {
 
 		// super.visit(node, param);
@@ -214,7 +231,8 @@ public class TypeChecking extends DefaultVisitor {
 		return null;
 	}
 
-	//	class SentenceIf { Expression condition;  List<Sentence> ifSentences;  List<Sentence> elseSentences; }
+	// class SentenceIf { Expression condition; List<Sentence> ifSentences;
+	// List<Sentence> elseSentences; }
 	public Object visit(SentenceIf node, Object param) {
 
 		// super.visit(node, param);
@@ -223,17 +241,21 @@ public class TypeChecking extends DefaultVisitor {
 			node.getCondition().accept(this, param);
 
 		if (node.getIfSentences() != null)
-			for (Sentence child : node.getIfSentences())
+			for (Sentence child : node.getIfSentences()) {
+				child.setFatherFunction(node.getFatherFunction());
 				child.accept(this, param);
+			}
 
 		if (node.getElseSentences() != null)
-			for (Sentence child : node.getElseSentences())
+			for (Sentence child : node.getElseSentences()) {
+				child.setFatherFunction(node.getFatherFunction());
 				child.accept(this, param);
+			}
 
 		return null;
 	}
 
-	//	class SentenceWhile { Expression condition;  List<Sentence> sentences; }
+	// class SentenceWhile { Expression condition; List<Sentence> sentences; }
 	public Object visit(SentenceWhile node, Object param) {
 
 		// super.visit(node, param);
@@ -242,51 +264,81 @@ public class TypeChecking extends DefaultVisitor {
 			node.getCondition().accept(this, param);
 
 		if (node.getSentences() != null)
-			for (Sentence child : node.getSentences())
+			for (Sentence child : node.getSentences()) {
+				child.setFatherFunction(node.getFatherFunction());
 				child.accept(this, param);
+			}
 
 		return null;
 	}
 
-	//	class ExpressionConstantInt { String value; }
+	// class ExpressionConstantInt { String value; }
 	public Object visit(ExpressionConstantInt node, Object param) {
+
+		node.setType(TypeInt.getInstance());
+
+		node.setModifiable(false);
+
 		return null;
 	}
 
-	//	class ExpressionConstantFloat { String value; }
+	// class ExpressionConstantFloat { String value; }
 	public Object visit(ExpressionConstantFloat node, Object param) {
+
+		node.setType(TypeFloat.getInstance());
+
+		node.setModifiable(false);
+
 		return null;
 	}
 
-	//	class ExpressionConstantChar { String value; }
+	// class ExpressionConstantChar { String value; }
 	public Object visit(ExpressionConstantChar node, Object param) {
+
+		node.setType(TypeChar.getInstance());
+
+		node.setModifiable(false);
+
 		return null;
 	}
 
-	//	class ExpressionCallFunction { String name;  List<Expression> callFunctionParams; }
+	// class ExpressionCallFunction { String name; List<Expression>
+	// callFunctionParams; }
 	public Object visit(ExpressionCallFunction node, Object param) {
 
+		
 		// super.visit(node, param);
 
 		if (node.getCallFunctionParams() != null)
 			for (Expression child : node.getCallFunctionParams())
 				child.accept(this, param);
 
+		node.setType(node.getDefinition().getType());
+
+		node.setModifiable(false);
+
+
+
 		return null;
 	}
 
-	//	class ExpressionUnary { String operator;  Expression expression; }
+	// class ExpressionUnary { String operator; Expression expression; }
 	public Object visit(ExpressionUnary node, Object param) {
-
 		// super.visit(node, param);
 
 		if (node.getExpression() != null)
 			node.getExpression().accept(this, param);
 
+		node.setType(TypeInt.getInstance());
+
+		node.setModifiable(false);
+
+
+
 		return null;
 	}
 
-	//	class ExpressionCast { Type newType;  Expression expression; }
+	// class ExpressionCast { Type newType; Expression expression; }
 	public Object visit(ExpressionCast node, Object param) {
 
 		// super.visit(node, param);
@@ -297,12 +349,16 @@ public class TypeChecking extends DefaultVisitor {
 		if (node.getExpression() != null)
 			node.getExpression().accept(this, param);
 
+		node.setType(node.getNewType());
+
+		node.setModifiable(false);
+
 		return null;
 	}
 
-	//	class ExpressionArithmetic { Expression left;  String operator;  Expression right; }
+	// class ExpressionArithmetic { Expression left; String operator; Expression
+	// right; }
 	public Object visit(ExpressionArithmetic node, Object param) {
-
 		// super.visit(node, param);
 
 		if (node.getLeft() != null)
@@ -310,11 +366,17 @@ public class TypeChecking extends DefaultVisitor {
 
 		if (node.getRight() != null)
 			node.getRight().accept(this, param);
+		node.setType(node.getLeft().getType());
+
+		node.setModifiable(false);
+
+
 
 		return null;
 	}
 
-	//	class ExpressionRelational { Expression left;  String operator;  Expression right; }
+	// class ExpressionRelational { Expression left; String operator; Expression
+	// right; }
 	public Object visit(ExpressionRelational node, Object param) {
 
 		// super.visit(node, param);
@@ -325,10 +387,15 @@ public class TypeChecking extends DefaultVisitor {
 		if (node.getRight() != null)
 			node.getRight().accept(this, param);
 
+		node.setType(TypeInt.getInstance());
+
+		node.setModifiable(false);
+
 		return null;
 	}
 
-	//	class ExpressionLogical { Expression left;  String operator;  Expression right; }
+	// class ExpressionLogical { Expression left; String operator; Expression right;
+	// }
 	public Object visit(ExpressionLogical node, Object param) {
 
 		// super.visit(node, param);
@@ -339,28 +406,40 @@ public class TypeChecking extends DefaultVisitor {
 		if (node.getRight() != null)
 			node.getRight().accept(this, param);
 
+		node.setType(TypeInt.getInstance());
+
+		node.setModifiable(false);
+
 		return null;
 	}
 
-	//	class ExpressionVariable { String name; }
+	// class ExpressionVariable { String name; }
 	public Object visit(ExpressionVariable node, Object param) {
+
+		node.setType(node.getDefinition().getType());
+
+		node.setModifiable(true);
+
 		return null;
 	}
 
-	//	class ExpressionStructField { Expression struct;  String name; }
+	// class ExpressionStructField { Expression struct; String name; }
 	public Object visit(ExpressionStructField node, Object param) {
-
 		// super.visit(node, param);
 
 		if (node.getStruct() != null)
 			node.getStruct().accept(this, param);
 
+		node.setType(node.getStruct().getType());
+
+		node.setModifiable(true);
+
+
 		return null;
 	}
 
-	//	class ExpressionArray { Expression array;  Expression index; }
+	// class ExpressionArray { Expression array; Expression index; }
 	public Object visit(ExpressionArray node, Object param) {
-
 		// super.visit(node, param);
 
 		if (node.getArray() != null)
@@ -369,31 +448,28 @@ public class TypeChecking extends DefaultVisitor {
 		if (node.getIndex() != null)
 			node.getIndex().accept(this, param);
 
+		//TODO:type=array.type.type
+		//IO??¿
+		node.setType(node.getArray().getType());
+
+		node.setModifiable(true);
+
+
 		return null;
 	}
 
+	private void predicado(boolean condition, String errorMessage, AST node) {
+		predicado(condition, errorMessage, node.getStart());
+	}
 
+	private void predicado(boolean condition, String errorMessage, Position position) {
+		if (!condition)
+			errorManager.notify("Type Checking", errorMessage, position);
+	}
 
+	private void predicado(boolean condition, String errorMessage) {
+		predicado(condition, errorMessage, (Position) null);
+	}
 
-
-
-
-
-
-
-
-    private void predicado(boolean condition, String errorMessage, AST node) {
-        predicado(condition, errorMessage, node.getStart());
-    }
-
-    private void predicado(boolean condition, String errorMessage, Position position) {
-        if (!condition)
-            errorManager.notify("Type Checking", errorMessage, position);
-    }
-
-    private void predicado(boolean condition, String errorMessage) {
-        predicado(condition, errorMessage, (Position) null);
-    }
-
-    private ErrorManager errorManager;
+	private ErrorManager errorManager;
 }
